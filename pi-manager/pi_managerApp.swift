164265804f2d83ed4e6608b939fb497a7f533f8e
acorvi <146776449+acorvi@users.noreply.github.com>
2026-04-29 14:03:19 +0100

    Initial Commit

---
+++ pi-manager/pi_managerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pi_managerApp.swift
+//  pi-manager
+//
+//  Created by Andrea Corvi on 29/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct pi_managerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
